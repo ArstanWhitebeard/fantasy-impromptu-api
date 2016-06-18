78b632a0b43cd30a132c6f3632599a71998159e9
ArstanWhitebeard <ArstanWhitebeard@users.noreply.github.com>
2016-06-18 12:29:16 +0100

    Add basic API for team standings

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var app = express();
+var mysql      = require('mysql');
+
+// TODO pull out config.js
+// TODO set up read-only db user!
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'olympics2016'
+});
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  connection.connect();
+
+  connection.query('SELECT * from team_standings ORDER BY points DESC, golds DESC, silvers DESC', function(err, rows, fields) {
+    if (!err)
+      res.send(rows);
+    else
+      res.send([]);
+  });
+
+  connection.end();
+
 });
 
 app.listen(3000, function () {
